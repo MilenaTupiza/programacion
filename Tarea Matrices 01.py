e9f8da5a7ee8c27a5d253366f1196f15324fc81a
josselyn21 <66844876+josselyn21@users.noreply.github.com>
2020-08-06 15:55:48 -0500

    Add files via upload

---
+++ Tarea Matrices 01.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 01:29:14 2020
+
+@author: USA
+"""
+
+
+import numpy as np
+# ingrese un valor para fila y columna de la matriz...
+# ...detro de los parentesis.
+m=np.random.random((4,4))
+print(m)
+print("\n"*2)
+matrix=np.eye((m))
+print(matrix)
